4c11a0bc91700fd84c3dea7eecd0f45f3ee29f91
sumit0-70 <sumitkumarsk23062004@gmail.com>
2025-08-10 14:16:30 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
